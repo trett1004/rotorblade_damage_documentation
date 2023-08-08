8c27dbe055c2a4f7924fd509a35a849e7caddfa7
Max <maxtretter@tutanota.com>
2023-08-08 10:34:56 +0200

    set up express server and first post request of damageNr

--- client/src/components/DamageNr.js
+++ client/src/components/DamageNr.js
@@ -1,21 +1,30 @@
 import React, { useState } from'react';
+import axios from 'axios';
 
 
 function DamageNr() {
     const [damageNr, setDamageNr] = useState(0);
+
+    const handleSubmit = () => {
+
+        axios.post('/api', {damageNr: parseInt(damageNr)})
+        .then(console.log("test", typeof(damageNr)))
+        .then((res) => console.log('res', res.data))
+        .catch(error => console.error('Error sending data', error));
+    }
   
     return (
         <div className="damageNr">
           <label>Damage Number</label>
           <br></br>
           <input
-            type='text'
+            type='number'
             value={damageNr}
             onChange={(e) => setDamageNr(e.target.value)}
             required
           /><br></br>
           <h1>DamageNr: {damageNr}</h1>
-          <button onClick={() => setDamageNr(damageNr + 1)}>Save</button>
+          <button onClick={handleSubmit}>Save</button>
         </div>
   
     );
---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const fs = require('fs').promises;
+const app = express()
+const port = 3010;
+app.use(express.json())
+
+app.post('/api', (req, res) => {
+    const damageNr = req.body;
+    console.log('req', req.body)
+    fs.readFile('database.json', 'utf8')
+    .then((data) => {
+        return JSON.parse(data)
+    })
+    .then((data) => {
+        if (!data) throw new Error('Error reading file'); // Throw an error to stop the chain
+        return data;
+    })
+    .then((data) => {
+        data.push(damageNr)
+        return data;
+    })
+    .then((data) => {
+        fs.writeFile('database.json', JSON.stringify(data))
+        return data;
+    })
+    .then((data) => {
+        console.log('data', data)
+        return res.json({ message: 'Damage received successfully', data: data })
+    })
+
+    .catch((err) => console.log('Error writing file',err));
+});
+
+
+app.listen(port, () => console.log('Server ready on 3010'))
